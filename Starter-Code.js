f406542d12b149513d59cb1a930a56af41052c31
aonnadi <aninja1008@gmail.com>
2023-10-25 21:08:58 -0400

    first commit

---
+++ Starter-Code.js
@@ -0,0 +1,18 @@
+//Starter Code
+
+function draw() {
+  background(220);
+  line(100, 200, 20, 75);
+describe(
+  'a 100 pixels long line running from mid-top to bottom-right of canvas - Syntax: line(x1, y1, x2, y2)'
+);
+  rect(10,20,25,20)
+  describe(
+  'a 25 pixel x 20 pixel rectangle in the top left of the canvas'
+);
+  triangle(30, 60, 90, 60, 60, 10)
+  describe(
+  'a triangle in the top left of the canvas'
+);
+  
+}
